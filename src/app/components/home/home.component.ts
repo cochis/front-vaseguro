ed42790bcd3f39237993237eab61900d5f1d6280
Oscar Alejandro Ramirez Rosas <ing.oarrs@gmail.com>
2019-05-18 10:55:15 -0500

    3ero

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,38 +15,23 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    
-    this.cotizacion = localStorage.getItem('init');
-   console.log(this.cotizacion); // true
-
-  // console.log(typeof locura); // 'string'
-  //   this.cotizacion = this.getLocal('init');
-    
-      // console.log(this.cotizacion);
-      // console.log(JSON.stringify(locura));
-    if(this.cotizacion){
+ 
       this.display = true;
-    }else {
-      this.display= false;
-
-    }
+    
     
   }
 
   showDialog() {
-    localStorage.setItem('init', false);
+    // localStorage.setItem('init', false);
     
-    // if (this.getLocal('init') === true ) {
-    //   this.display = true;
-    //   this.setLocal('init', this.init);
-    // }
+   
   }
   cotiza() {
-    localStorage.setItem('init', false);
+    // localStorage.setItem('init', false);
     this.display = false;
   }
   noCotiza() {
-    localStorage.setItem('init', false);
+    // localStorage.setItem('init', false);
     this.display = false;
     
   }
